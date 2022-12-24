1a9c6fd95fe581c315a22336569a4adab2d7d660
ValkopDev <100997239+ValkopDev@users.noreply.github.com>
2022-12-24 16:57:52 +0200

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,8 @@
+-- Not done yet!
+-- Wait for it to come out soon!
+	game.StarterGui:SetCore("SendNotification",{
+		Title = "Game hub not done yet!";
+		Text = "plz wait kthx";
+		Icon = "";
+		Duration = 5;
+	})
